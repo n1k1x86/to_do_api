7ae529ce266c54438337dfe9ef82cff92a0587f7
n1k1x86 <i@n1k1x86.ru>
2025-05-13 17:25:38 +1000

    created config reader

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	ToDoDBConfig ToDoDB `yaml:"to_do_db"`
+}
+
+type ToDoDB struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Server   string `yaml:"server"`
+	DBName   string `yaml:"db_name"`
+}
+
+func (t *ToDoDB) BuildDSN() string {
+	return fmt.Sprintf(`postgres://%s:%s@%s/%s?sslmode=disable`, t.Username, t.Password, t.Server, t.DBName)
+}
+
+func NewConfig() (*Config, error) {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		return nil, err
+	}
+	cfg := &Config{}
+	fmt.Println(data)
+	err = yaml.Unmarshal(data, cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
